f5ae07b8960a23bc5f5610f05e98586e8b2051ab
Лев Романович Панас <jieba2005@list.ru>
2025-12-18 16:44:12 +0300

    проверка 3

--- backend/game_journal/settings.py
+++ backend/game_journal/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from pathlib import Path
-from keys import DJANGO_SECRET_KEY
+from keysecret import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
